d45be353ef728dacfe3f613a522d2dec52b3a50e
Vk <venkatkumar2020ece@gmail.com>
2023-06-18 19:26:18 +0530

    pdf2Audio conversion

---
+++ Pdf2Audio_conversion/Audiobook.py
@@ -0,0 +1,18 @@
+#!pip install -r requirements.txt
+
+#import library
+import pdfplumber as pp
+from gtts import gTTS
+from tqdm import tqdm
+
+#Extract text from pdf
+pdf_text = ''
+
+with pp.open('book.pdf') as pdf:
+    for page in tqdm(pdf.pages):
+        pdf_text += page.extract_text()
+
+#convert extracted text to speech
+tts = gTTS(text=pdf_text, lang='en')
+tts.save('audio_rl_book.mp3')
+
