4aea8108e7cec499238404fbf9a08bcf4683c83b
Vk <venkatkumar2020ece@gmail.com>
2023-06-18 20:52:40 +0530

    One new added

--- Pdf2Audio_conversion/Audiobook.py
+++ Pdf2Audio_conversion/Audiobook.py
@@ -8,11 +8,11 @@ from tqdm import tqdm
 #Extract text from pdf
 pdf_text = ''
 
-with pp.open('book.pdf') as pdf:
+with pp.open('HDE.pdf') as pdf:
     for page in tqdm(pdf.pages):
         pdf_text += page.extract_text()
 
 #convert extracted text to speech
 tts = gTTS(text=pdf_text, lang='en')
-tts.save('audio_rl_book.mp3')
+tts.save('HDE.mp3')
 
